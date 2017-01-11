39f230261147c324a56e279f1a0f66b64234ebd4
Nick Blaisdell <nick.blaisdell@gmail.com>
2017-01-11 13:24:31 -0500

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :board_game_collections
+  has_many :board_games, through: :board_game_collections
+
+  validates :email, uniqueness: true
+  validates :role, inclusion: { in: ['user', 'admin'] }
+end
